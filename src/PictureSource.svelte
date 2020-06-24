47da3665b6bf543bc0a69f038bf6e9f521ffe04d
Peter-Paul van Gemerden <mail@ppvg.nl>
2020-06-24 23:44:14 +0200

    fix: log PictureSource fallback error only once

--- src/PictureSource.svelte
+++ src/PictureSource.svelte
@@ -1,20 +1,21 @@
-<script>
-	/* This component is not supposed to end up in your runtime. Normally, the
-	preprocessor would replace it with a native <source>, converting/resizing the
-	file referenced by the custom `original` prop to generate the files in
-	`srcset`. */
-	export let original
+<script context="module">
+	/* This component is not supposed to end up in your runtime. */
 	console.error(
-		`Error: <PictureSource original="${original}" ... /> was not converted by the svelte-picture-source preprocessor. Please check your build process.`
+		'The svelte-picture-source preprocessor did not run. Please check your build process.'
 	)
+</script>
 
+<script>
 	/* If this does end up in your runtime, as a fallback, we render a native
-	`<source>` with all of its native attributes. Note that the files referenced
-	by `srcset` may not exist. */
-	export let media
-	export let sizes
+	`<source>` with all of its native attributes. Note that, because the
+	preprocessor didn't run, the files referenced by `srcset` may not exist. */
+	export let original
 	export let srcset
-	export let type
+	export let media = undefined
+	export let sizes = undefined
+	export let type = undefined
+	// silence "unused export property" compiler error
+	$: original
 </script>
 
 <source {media} {sizes} {srcset} {type} />
